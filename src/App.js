43c76a44d36a5c3590288995257b34920da8fa2c
Typonote <kimradium@naver.com>
2021-11-07 16:15:19 +0900

    #5 App에서 todos상태 이용하기 (p.270 ~)

--- src/App.js
+++ src/App.js
@@ -1,14 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
 import TodoTemplate from './components/TodoTemplate';
 import TodoInsert from './components/TodoInsert';
 import TodoList from './components/TodoList';
+import { useState } from 'react';
 
 function App() {
+  const [todos, setTodos] = useState([
+    {
+      id: 1,
+      text: '겉옷 빨래하기',
+      checked: true,
+    },
+    {
+      id: 2,
+      text: '책상 정리',
+      checked: true,
+    },
+    {
+      id: 3,
+      text: '부모님 안부전화 드리기',
+      checked: false,
+    },
+  ]);
+
   return (
     <TodoTemplate>
       <TodoInsert />
-      <TodoList />
+      <TodoList todos={todos} />
     </TodoTemplate>
   );
 }
--- src/components/TodoListItem.js
+++ src/components/TodoListItem.js
@@ -8,12 +8,15 @@ import cn from 'classnames';
 import '../styles/_TodoListItem.scss';
 
 // 각 할일 항목에 대한 정보를 보여주는 컴포넌트 => todo 객체를 props로 받아와서 상태에 따른 다른 스타일의 UI를 보여줌.
-const TodoListItem = () => {
+const TodoListItem = ({ todo }) => {
+  // 값은 app.js의 todos의 기본 값에 자장되어 있다.
+  const { text, checked } = todo;
+
   return (
     <div className="TodoListItem">
-      <div className="checkbox">
-        <MdCheckBoxOutlineBlank />
-        <div className="text">할일</div>
+      <div className={cn('checkbox', { checked })}>
+        {checked ? <MdCheckBox /> : <MdCheckBoxOutlineBlank />}
+        <div className="text">{text}</div>
       </div>
       <div className="remove">
         <MdRemoveCircleOutline />
