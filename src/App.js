2c90748a4cb54ccd0cf914f6b0d385b1a4ca3512
Typonote <kimradium@naver.com>
2021-11-07 20:07:17 +0900

    #6 항목 추가 기능 구현하기 (p.274 ~)

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import TodoTemplate from './components/TodoTemplate';
 import TodoInsert from './components/TodoInsert';
 import TodoList from './components/TodoList';
-import { useState } from 'react';
+import { useCallback, useRef, useState } from 'react';
 
 function App() {
   const [todos, setTodos] = useState([
@@ -23,9 +23,25 @@ function App() {
     },
   ]);
 
+  // id 값은 렌더링 되는 정보가 아니기 때문에, useRef사용
+  const nextId = useRef(4);
+
+  const OnInsertHandler = useCallback(
+    (text) => {
+      const todo = {
+        id: nextId.current,
+        text,
+        checked: false,
+      };
+      setTodos(todos.concat(todo));
+      nextId.current += 1; // nextId 1 씩 더하기
+    },
+    [todos],
+  );
+
   return (
     <TodoTemplate>
-      <TodoInsert />
+      <TodoInsert onInsert={OnInsertHandler} />
       <TodoList todos={todos} />
     </TodoTemplate>
   );
--- src/components/TodoInsert.js
+++ src/components/TodoInsert.js
@@ -3,15 +3,28 @@ import { MdAdd } from 'react-icons/md';
 import '../styles/_TodoInsert.scss';
 
 // 새로운 항목을 입력하고 추가할 수 있는 컴포넌트
-const TodoInsert = () => {
+const TodoInsert = ({ onInsert }) => {
   const [value, setValue] = useState('');
+
   // 함수를 재사용하고, 렌더링을 최적화 하기 위해 useCallback을 사용.
   const onChangeHandler = useCallback((e) => {
     setValue(e.target.value);
   }, []);
 
+  const OnSubmitHandler = useCallback(
+    (e) => {
+      onInsert(value);
+      setValue(''); // value 값 초기화
+
+      // submit 이벤트는 브라우저에서 새로고침을 발생시킵니다.
+      // 이를 방지하기 위하여 preventDefault() 함수를 호출.
+      e.preventDefault();
+    },
+    [onInsert, value],
+  );
+
   return (
-    <form className="TodoInsert">
+    <form className="TodoInsert" onSubmit={OnSubmitHandler}>
       <input
         placeholder="할 일을 입력하세요"
         value={value}
